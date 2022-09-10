2dbc3c537dbc500daf9073512e1fb03f3cc65714
Mordi490 <jananton007@hotmail.com>
2022-09-10 12:13:32 +0200

    added skeletons for TOS & PP

---
+++ src/pages/privacy-policy.tsx
@@ -0,0 +1,11 @@
+const Privacy = () => {
+
+  return (
+    <>
+    <h1>Yo, Discord might also like this</h1>
+    <p>TODO: </p>
+    </>
+  )
+}
+
+export default Privacy;
---
+++ src/pages/terms-of-service.tsx
@@ -0,0 +1,10 @@
+const TOS = () => {
+  return (
+    <>
+      <h1>Yo, discord might like this</h1>
+      <p>TODO: </p>
+    </>
+  );
+};
+
+export default TOS;
