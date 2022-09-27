a90629d3da1f3a9088b460cd4b0bd6e12b0267ec
Mordi490 <jananton007@hotmail.com>
2022-09-27 16:21:03 +0200

    Stashed changes

--- src/pages/[id]/lineups.tsx
+++ src/pages/[id]/lineups.tsx
@@ -1,42 +1,55 @@
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import { trpc } from "../../utils/trpc";
-import Image from "next/image";
 import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import Layout from "../../../components/layout";
+import Loading from "../../../components/loading";
+import { trpc } from "../../utils/trpc";
 
 const Lineups = () => {
-  const { data: session } = useSession();
   const id = useRouter().query.id as string;
 
   const { data: userInfo } = trpc.useQuery(["user.get-user", { id }]);
 
-  // fetch own lineups
+  // fetch the user's lineups
   const { data: lineups, isLoading } = trpc.useQuery([
     "lineup.by-author",
     { id },
   ]);
 
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <>
+      <Layout text={`${userInfo?.name}`} title={`${userInfo?.name}`}>
+        <h1 className="text-center text-lg">
+          Here are all the lineups for the user: {userInfo?.name}
+        </h1>
+        {userInfo?.image && (
+          <Image
+            src={userInfo?.image}
+            height={64}
+            width={64}
+            alt="Discord profile picture"
+          />
+        )}
+        <p>Display high lvl info: # lineups, total votes, etc </p>
+        <ul className="container">
+          {lineups?.length ? (
+            lineups.map((lineup) => <li key={lineup.id}>{lineup.title}</li>)
+          ) : (
+            <p className="text-lg">This user does not have any lineups</p>
+          )}
+        </ul>
+
+        <h1>hehehe remove l8er</h1>
+        <hr className="my-8" />
+        <pre className="block text-sm">{JSON.stringify(lineups, null, 4)}</pre>
+      </Layout>
       <Head>
         <title>{`${userInfo?.name}'s lineups`}</title>
       </Head>
-      <h1 className="text-center text-lg">
-        Here are all the lineups for the user: {userInfo?.name}
-      </h1>
-      {userInfo?.image && (
-        <Image
-          src={userInfo?.image}
-          height={64}
-          width={64}
-          alt="Discord profile picture"
-        />
-      )}
-      <p>Display high lvl info: # lineups, total votes, etc </p>
-
-      <h1>hehehe remove l8er</h1>
-      <hr className="my-8" />
-      <pre>{JSON.stringify(lineups, null, 4)}</pre>
     </>
   );
 };
