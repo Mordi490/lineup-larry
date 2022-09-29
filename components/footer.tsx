f7c4938c661515641e92de39eeb254532b1a158f
Mordi490 <jananton007@hotmail.com>
2022-09-29 11:42:49 +0200

    Layout & footer fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,32 +2,26 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 pt-4">
-      <div className="grid grid-cols-3">
-        <div>
-          <Link href="/faq">
-            <a>FAQ</a>
-          </Link>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </div>
-        <div>
-          <Link href="/terms-of-service">
-            <a>Terms of Service</a>
-          </Link>
-          <Link href="/contact">
-            <a>Contact</a>
-          </Link>
-        </div>
-        <div>
-          <Link href="/privacy-policy">
-            <a>Privacy Policy</a>
-          </Link>
-          <Link href="https://github.com/Mordi490/lineup-larry">
-            <a>GitHub</a>
-          </Link>
-        </div>
+    <footer className="bg-gray-800 py-4 text-center">
+      <div className="grid grid-cols-3 gap-4">
+        <Link href="/faq">
+          <a>FAQ</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+        <Link href="/terms-of-service">
+          <a>Terms of Service</a>
+        </Link>
+        <Link href="/contact">
+          <a>Contact</a>
+        </Link>
+        <Link href="/privacy-policy">
+          <a>Privacy Policy</a>
+        </Link>
+        <Link href="https://github.com/Mordi490/lineup-larry">
+          <a>GitHub</a> 
+        </Link>
       </div>
     </footer>
   );
