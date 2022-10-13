9ee2481c78cfe80aee1e134debca55a0ca25ff0b
Mordi490 <jananton007@hotmail.com>
2022-10-13 14:20:53 +0200

    pagination - QoL changes

--- src/pages/lineups.tsx
+++ src/pages/lineups.tsx
@@ -6,9 +6,28 @@ import Link from "next/link";
 import { trpc } from "../utils/trpc";
 import Loading from "../../components/loading";
 import Layout from "../../components/layout";
+import { Fragment } from "react";
 
 const Lineups: NextPage = () => {
-  const { data, isLoading, isError, error } = trpc.useQuery(["lineup.get-all"]);
+  const {
+    data: paginatedData,
+    hasPreviousPage,
+    isFetchingPreviousPage,
+    fetchPreviousPage,
+    isLoading,
+    isError,
+    error,
+  } = trpc.useInfiniteQuery(
+    [
+      "lineup.infiniteLineups",
+      {
+        limit: 12,
+      },
+    ],
+    {
+      getPreviousPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
 
   if (isLoading) {
     return <Loading />;
@@ -22,37 +41,47 @@ const Lineups: NextPage = () => {
     <Layout>
       <h1 className="my-2 text-center text-4xl">Lineups</h1>
       <div className="mx-4 grid gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {data?.length ? (
-          data.map((lineup) => (
-            <section key={lineup.id}>
-              <Link href={`/lineup/${lineup.id}`}>
-                <a>{lineup.title}</a>
-              </Link>
-              <p>
-                by:
-                <Link href={`/user/${lineup.userId}`}>
-                  <a className="ml-1 text-sky-500 underline">
-                    {lineup.creator}
+        {paginatedData?.pages.map((page, index) => (
+          <Fragment key={page.items[0]?.id || index}>
+            {page.items.map((lineup) => (
+              <article key={lineup.id}>
+                <Link href={`/lineup/${lineup.id}`}>
+                  <a>{lineup.title}</a>
+                </Link>
+                <p>
+                  by:
+                  <Link href={`/user/${lineup.userId}`}>
+                    <a className="ml-1 text-sky-500 underline">
+                      {lineup.creator}
+                    </a>
+                  </Link>
+                </p>
+                <Link href={`/lineup/${lineup.id}`}>
+                  <a>
+                    <Image
+                      src={`https://t3-larry-bucket.s3.eu-west-2.amazonaws.com/${lineup.image}`}
+                      alt="lineup"
+                      width={1280}
+                      height={720}
+                    />
                   </a>
                 </Link>
-              </p>
-
-              <Link href={`/lineup/${lineup.id}`}>
-                <a>
-                  <Image
-                    src={`https://t3-larry-bucket.s3.eu-west-2.amazonaws.com/${lineup.image}`}
-                    alt="lineup"
-                    width={1280}
-                    height={720}
-                  />
-                </a>
-              </Link>
-            </section>
-          ))
-        ) : (
-          <div>There are currently no lineups</div>
-        )}
+              </article>
+            ))}
+          </Fragment>
+        ))}
       </div>
+      <button
+        onClick={() => fetchPreviousPage()}
+        disabled={!hasPreviousPage || isFetchingPreviousPage}
+        className="flex h-fit w-fit justify-end rounded-lg bg-blue-600 px-8 py-4 font-semibold uppercase text-white disabled:bg-gray-100 disabled:text-gray-400"
+      >
+        {isFetchingPreviousPage
+          ? "Loading more..."
+          : hasPreviousPage
+          ? "Load More"
+          : "Nothing more to load"}
+      </button>
     </Layout>
   );
 };
