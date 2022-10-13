1158023ab8f95d9bd84223adddd5f7cdd5105938
Mordi490 <jananton007@hotmail.com>
2022-10-13 16:22:57 +0200

    Removed apostrophe, apostrophe bad

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,9 +10,9 @@ const About = () => {
           also aims to be a different experience than lineup guides on YouTube
           or other video based platforms. This site will be primarily be picture
           based. There are a few reasons for this, but the biggest one is that
-          its's faster than jumping between timelines on a video. Once you know
-          ~s80% of a lineup, you only need a picture or two to confirm placement
-          of character and crosshair placement.
+          it&apos;s faster than jumping between timelines on a video. Once you
+          know ~s80% of a lineup, you only need a picture or two to confirm
+          placement of character and crosshair placement.
         </p>
       </div>
     </Layout>
