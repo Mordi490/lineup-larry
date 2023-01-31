0eb4555c7a476eff5120822e0cbc6bf4776d58e6
Mordi490 <jananton007@hotmail.com>
2023-01-31 14:58:05 +0100

    added small "guide" on SS & recordings

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -5,15 +5,23 @@ import Layout from "../components/layout";
 const FAQ = () => {
   return (
     <Layout title="FAQ">
-      <div className="mx-auto mt-4 max-w-2xl">
-        <h1 className="my-4 text-center text-2xl font-bold">
+      <div className="mx-auto mt-4 max-w-2xl space-y-4">
+        <h1 className="text-center text-2xl font-bold">
           Why does this exist? (TLDR version)
         </h1>
-        <p className="my-4">For fun</p>
-        <h1 className="my-4 text-center text-2xl font-bold">
+        <p>This is sort of a side project, so for fun I guess</p>
+        <h1 className="text-center text-2xl font-bold">How do I help</h1>
+        <p>
+          By uploading lineups, take a look at the
+          <Link href="/guide" className="ml-1 font-medium underline">
+            guide
+          </Link>{" "}
+          that gives advice around tools & settings for screenshots & recordings
+        </p>
+        <h1 className=" text-center text-2xl font-bold">
           Does this side project have goals
         </h1>
-        <p className="my-4">
+        <p>
           Nothing, specific. I wanted to get better at TS in general, and the
           <Link
             href="https://create.t3.gg/"
